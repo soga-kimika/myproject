ae8174a56875f3c0603334f69c75be95e89805c0
soga-kimika <170947386+soga-kimika@users.noreply.github.com>
2024-09-29 01:29:59 +0900

    ホーム画面CSS

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -37,12 +37,15 @@
 
 body {
     background: 
-    radial-gradient(at 40% 20%, hsla(28, 100%, 88%, 1) 0px, transparent 50%),
-    radial-gradient(at 80% 0%, hsla(189, 100%, 88%, 1) 0px, transparent 50%),
-    radial-gradient(at 0% 50%, hsla(355, 100%, 88%, 1) 0px, transparent 50%),
-    radial-gradient(at 80% 50%, hsla(340, 100%, 88%, 1) 0px, transparent 50%),
-    radial-gradient(at 0% 100%, hsla(22, 100%, 88%, 1) 0px, transparent 50%),
-    radial-gradient(at 80% 100%, hsla(242, 100%, 88%, 1) 0px, transparent 50%);
+    radial-gradient(at 40% 20%, rgb(232, 12, 12) 0px, transparent 50%),
+    radial-gradient(at 40% 20%, hsla(28, 100%, 74%, 1) 0px, transparent 50%),
+    radial-gradient(at 80% 0%, hsla(189, 100%, 56%, 1) 0px, transparent 50%),
+    radial-gradient(at 0% 50%, hsla(355, 100%, 93%, 1) 0px, transparent 50%),
+    radial-gradient(at 80% 50%, hsla(340, 100%, 76%, 1) 0px, transparent 50%),
+    radial-gradient(at 0% 100%, hsla(22, 100%, 77%, 1) 0px, transparent 50%),
+    radial-gradient(at 80% 100%, hsla(242, 100%, 70%, 1) 0px, transparent 50%);
+
+
 
   background-position: center center;
   align-items: center;
