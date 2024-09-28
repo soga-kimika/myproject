78faabf00e35690cecf535e2c48b7ff60e5bd711
soga-kimika <170947386+soga-kimika@users.noreply.github.com>
2024-09-28 20:23:36 +0900

    不要のラジオボタンを追加

--- resources/views/partials/form_modal.blade.php
+++ resources/views/partials/form_modal.blade.php
@@ -1,7 +1,6 @@
 <div class="container">
     <div class="col-md-12 mx-auto"> 
         {{-- 入力フォーム --}}
-        @if($title2 !== 'ナッシング')
         <div class="col-md-12 d-flex mb-3">
                 <label class="mb-0">優先度：</label>
                 <div class="d-flex">
@@ -17,9 +16,15 @@
                         <input type="radio" class="form-check-input" name="priority" value="low" id="priority-low" {{ $item->priority == 'low' ? 'checked' : '' }}>
                         <label class="form-check-label" for="priority-low">低 <i class="far fa-star"></i></label>
                     </div>
+                    {{-- アイディアのページだけ不要のラジオボタンを表示 --}}
+                    @if($type ==='ideas')
+                    <div class="form-check">
+                        <input type="radio" class="form-check-input" name="priority" value="remove" id="priority-remove"{{ $item->priority == 'remove' ? 'checked' : '' }}>
+                        <label class="form-check-label" for="priority-remove">不要 <i class="fa fa-times"></i></label>
+                    </div>
+                @endif
                 </div>
         </div>
-        @endif
         <div class="form-group d-flex align-items-center col-md-12">
             <input type="text" name="request_message" id="edit_request_message" class="form-control form-control-lg me-2" placeholder="要望を記入してください" 
             value="{{ $item->request_message }}" required style="flex: 1;">
