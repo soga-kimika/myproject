da18ddeacd16d108f729d191d62cea17b562967d
soga-kimika <170947386+soga-kimika@users.noreply.github.com>
2024-10-02 20:42:42 +0900

    ホーム画面のカウントダウンの表示を変更

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -1,11 +1,12 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use Illuminate\Http\Request;
 use App\Models\Client;
 
 
- //プロフィール情報をデータベースから取って表示    
+//プロフィール情報をデータベースから取って表示    
 class ClientController extends Controller
 {
     /**
@@ -17,116 +18,114 @@ class ClientController extends Controller
         $client = Client::first();
         // レコードがあったら、プロフィール情報を表示
         if ($client !== null) {
-            return view('client.index',compact('client'));
+            return view('client.index', compact('client'));
         } else {
-        // レコードかった場合、登録画面に遷移
+            // レコードかった場合、登録画面に遷移
             return redirect()->route('client.create');
         }
-     
-      
-     //プロフィールページを表示
-    } /**
-    * Show the form for creating a new resource.
-    */
-   public function create()
-   {
-       return view('client.create');
-   }
 
 
-      // クライアント情報を登録
+        //プロフィールページを表示
+    }
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('client.create');
+    }
+
+
+    // クライアント情報を登録
     /**
      * Store a newly created resource in storage.
      */
-   
+
     public function store(Request $request)
-{
-    // バリデーション
-    $request->validate([
-        'adult_count' =>'required|integer',
-        'child_count' =>'required|integer',
-        'pet' =>'required|string',
-        'land_budget_exists' => 'required|string',
-        'building_budget' =>'required|string',
-        'land_area' =>'required|string',
-        'building_area' =>'required|string',
-        'floors' =>'required|string',
-        'layout' =>'required|string',
-        'regularCars' =>'required|string',
-        'compactCars' =>'required|string',
-        'motorcycles' =>'required|string',
-        'bicycles' =>'required|string',
-        'construction_area' =>'required|string',
-        'date' =>'required|date',
-        'current_home_issues' =>'nullable|string',
-    ]);
+    {
+        // バリデーション
+        $request->validate([
+            'adult_count' => 'required|integer',
+            'child_count' => 'required|integer',
+            'pet' => 'required|string',
+            'land_budget_exists' => 'required|string',
+            'building_budget' => 'required|string',
+            'land_area' => 'required|string',
+            'building_area' => 'required|string',
+            'floors' => 'required|string',
+            'layout' => 'required|string',
+            'regularCars' => 'required|string',
+            'compactCars' => 'required|string',
+            'motorcycles' => 'required|string',
+            'bicycles' => 'required|string',
+            'construction_area' => 'nullable|string',
+            'date' => 'nullable|date',
+            'current_home_issues' => 'nullable|string',
+        ]);
 
-    // 土地の所有が「なし：no」の場合は、土地予算項目欄を必須とする
-    if ($request->land_budget_exists === 'no') {
-        $request['land_budget'] = 'required|string';
-    } else {
-        // 土地の所有が「ある：ｙｅｓ」の場合は、土地の予算項目は入力不要とする
-        $request['land_budget'] = 'nullable|string'; 
+        // 土地の所有が「なし：no」の場合は、土地予算項目欄を必須とする
+        if ($request->land_budget_exists === 'no') {
+            $request['land_budget'] = 'required|string';
+        } else {
+            // 土地の所有が「ある：ｙｅｓ」の場合は、土地の予算項目は入力不要とする
+            $request['land_budget'] = 'nullable|string';
+        }
+
+        // データの保存
+        $clients = $request->all();
+
+        // 土地の所有が「あり」の場合は、land_budgetに「-」を設定
+        if ($clients['land_budget_exists'] === 'yes') {
+            $clients['land_budget'] = '-';
+        }
+
+        Client::create($clients);
+
+        return redirect()->route('client.index');
     }
 
-    // データの保存
-    $clients = $request->all();
 
-    // 土地の所有が「あり」の場合は、land_budgetに「-」を設定
-    if ($clients['land_budget_exists'] === 'yes') {
-        $clients['land_budget'] = '-';
-    }
-
-    Client::create($clients);
-
-    return redirect()->route('client.index');
-}
-
-    
     // 編集
     /**
-         * Show the form for editing the specified resource.
-         */
-        public function edit()
-        {
-            //一番最初のレコードを取得
-            $client = Client::first();
-            return view('client.edit',compact('client'));
-        }
+     * Show the form for editing the specified resource.
+     */
+    public function edit()
+    {
+        //一番最初のレコードを取得
+        $client = Client::first();
+        return view('client.edit', compact('client'));
+    }
 
     // 更新
-        /**
-         * Update the specified resource in storage.
-         */
-        public function update(Request $request)
-        {
-            //バリデーション
-            $request->validate([
-                'adult_count' =>'nullable|integer',
-                'child_count' =>'nullable|integer',
-                'pet' =>'nullable|string',
-                'land_budget_exists' =>'nullable|numeric',
-                'building_budget' =>'nullable|numeric',
-                'land_area' =>'nullable|string',
-                'building_area' =>'nullable|string',
-                'floors' =>'nullable|string',
-                'layout' =>'nullable|string',
-                'regularCars' =>'nullable|string',
-                'miniCars' =>'nullable|string',
-                'motorcycles' =>'nullable|string',
-                'bicycles' =>'nullable|string',
-                'construction_area' =>'nullable|string',
-                'date' =>'nullable|date',
-                'current_home_issues' =>'nullable|string',
-            ]);
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request)
+    {
+        //バリデーション
+        $request->validate([
+            'adult_count' => 'nullable|integer',
+            'child_count' => 'nullable|integer',
+            'pet' => 'nullable|string',
+            'land_budget_exists' => 'nullable|numeric',
+            'building_budget' => 'nullable|numeric',
+            'land_area' => 'nullable|string',
+            'building_area' => 'nullable|string',
+            'floors' => 'nullable|string',
+            'layout' => 'nullable|string',
+            'regularCars' => 'nullable|string',
+            'miniCars' => 'nullable|string',
+            'motorcycles' => 'nullable|string',
+            'bicycles' => 'nullable|string',
+            'construction_area' => 'nullable|string',
+            'date' => 'nullable|date',
+            'current_home_issues' => 'nullable|string',
+        ]);
 
-                $client = Client::first();
+        $client = Client::first();
 
-                $client->update($request->all());
+        $client->update($request->all());
 
-                return redirect()->route('client.index');
-
-    
-        }
-    
+        return redirect()->route('client.index');
     }
+}
--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -8,94 +8,97 @@
 
 @section('content')
 <section class="content">
-  
+    <div class="row justify-content-center text-center">
+        <div class="col-auto">
+            <a href="{{ route('client.index') }}" title="プロフィール">
+                <i class="far fa-address-card home-icon"></i>
+            </a>
+        </div>
+        <div class="col-auto">
+            <a href="{{ route('items.index', 'ideas') }}" title="アイディア">
+                <i class="fas fa-lightbulb home-icon"></i>
+            </a>
+        </div>
+        <div class="col-auto">
+            <a href="{{ route('items.index', 'designs') }}" title="デザイン">
+                <i class="fas fa-palette home-icon"></i>
+            </a>
+        </div>
+        <div class="col-auto">
+            <a href="{{ route('items.index', 'ldk') }}" title="LDK">
+                <i class="fas fa-couch home-icon"></i>
+            </a>
+        </div>
+        <div class="col-auto">
+            <a href="{{ route('items.index', 'bathrooms') }}" title="バスルーム">
+                <i class="fas fa-sink home-icon"></i>
+            </a>
+        </div>
+        <div class="col-auto">
+            <a href="{{ route('items.index', 'rooms') }}" title="プライベートルーム">
+                <i class="fas fa-bed home-icon"></i>
+            </a>
+        </div>
+        <div class="col-auto">
+            <a href="{{ route('items.index', 'storages') }}" title="ストレージ">
+                <i class="fas fa-box home-icon"></i>
+            </a>
+        </div>
+        <div class="col-auto">
+            <a href="{{ route('galleries.index') }}" title="フォトギャラリー">
+                <i class="far fas fa-image home-icon"></i>
+            </a>
+        </div>
+    </div>
+    
+    <div id="target-date" style="display:none;">{{ $client ? $client->date : '' }}</div> 
+    <div class="countdown-container">
+        <div class="countdown-elm">
+            <h1>Countdown <span>to</span> My Home</h1>  
+            <h3>
+                <span>Desired Construction Date：</span>
+                {{ $client && $client->date ? $client->date : 'Not Set' }}
+            </h3>
+            <div class="countdown-display">
+                <span>Days Left</span>
+                <p class="time-text" id="days">0</p>
+                <span>days</span>
             </div>
-            <div class="row justify-content-center text-center">
-                <div class="col-auto">
-                    <a href="{{ route('client.index') }}" title="プロフィール">
-                        <i class="far fa-address-card home-icon"></i>
-                    </a>
-                </div>
-                <div class="col-auto">
-                    <a href="{{ route('items.index', 'ideas') }}" title="アイディア">
-                        <i class="fas fa-lightbulb home-icon"></i>
-                    </a>
-                </div>
-                <div class="col-auto">
-                    <a href="{{ route('items.index', 'designs') }}" title="デザイン">
-                        <i class="fas fa-palette home-icon"></i>
-                    </a>
-                </div>
-                <div class="col-auto">
-                    <a href="{{ route('items.index', 'ldk') }}" title="LDK">
-                        <i class="fas fa-couch home-icon"></i>
-                    </a>
-                </div>
-                <div class="col-auto">
-                    <a href="{{ route('items.index', 'bathrooms') }}" title="バスルーム">
-                        <i class="fas fa-sink home-icon"></i>
-                    </a>
-                </div>
-                <div class="col-auto">
-                    <a href="{{ route('items.index', 'rooms') }}" title="プライベートルーム">
-                        <i class="fas fa-bed home-icon"></i>
-                    </a>
-                </div>
-                <div class="col-auto">
-                    <a href="{{ route('items.index', 'storages') }}" title="ストレージ">
-                        <i class="fas fa-box home-icon"></i>
-                    </a>
-                </div>
-                <div class="col-auto">
-                    <a href="{{ route('galleries.index') }}" title="フォトギャラリー">
-                        <i class="far fas fa-image home-icon"></i>
-                    </a>
-                </div>
-            </div>
-            
-            <div id="target-date" style="display:none;">{{ $client->date }}</div> 
-            <div class="countdown-container">
-                <div class="countdown-elm">
-                     <h1>Countdown <span>to</span> My Home</h1>  
-                         <h3><span>Desired Construction Date：</span>{{ $client->date }}</h3>
-                            <div class="countdown-display">
-                                 <span>Days Left</span>
-                                 <p class="time-text" id="days" >0</p>
-                                <span>days</span>
-                            </div>
-                </div>
-            
-</div>
-
+        </div>
+    </div>
+    
+</section>
 @stop
 
 @section('css')
 <link rel="stylesheet" href="{{ asset('/css/item/index.css') }}">
-
 @stop
 
 @section('js')
 <script>
     const daysElm = document.getElementById('days');
-
-    // 建築希望日を取得
     const target = document.getElementById('target-date').innerText; 
-    const targetDate = new Date(target); // 日付に変換
 
-    function countDown() {
-        const currentDate = new Date();
-        const totalSeconds = (targetDate - currentDate) / 1000;
+    if (!target) {
+        daysElm.innerHTML = 'Not Set'; 
+    } else {
+        const targetDate = new Date(target); 
 
-        if (totalSeconds <= 0) {
-            daysElm.innerHTML = 0;
-            clearInterval(countdownInterval); // カウントダウンが終了したらクリア
-            return;
+        function countDown() {
+            const currentDate = new Date();
+            const totalSeconds = (targetDate - currentDate) / 1000;
+
+            if (totalSeconds <= 0) {
+                daysElm.innerHTML = 0;
+                clearInterval(countdownInterval); 
+                return;
+            }
+
+            const days = Math.floor(totalSeconds / 3600 / 24);
+            daysElm.innerHTML = days;
         }
 
-        const days = Math.floor(totalSeconds / 3600 / 24);
-        daysElm.innerHTML = days;
+        const countdownInterval = setInterval(countDown, 1000);
     }
-
-    const countdownInterval = setInterval(countDown, 1000);
 </script>
 @stop
