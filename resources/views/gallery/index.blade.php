ab3c8c675bc3adb62009fd32ac62c9878c231429
soga-kimika <170947386+soga-kimika@users.noreply.github.com>
2024-10-02 20:12:28 +0900

    ファイル名表示ボタンの位置を修正

--- resources/views/gallery/index.blade.php
+++ resources/views/gallery/index.blade.php
@@ -8,29 +8,8 @@
 
 @section('content')
 <div class="container">
-    <div class="form-group d-flex align-items-center col-md-12">
-        {{-- 画像投稿ボタン --}}
-        <label for="imageUpload" class="btn me-2 form-check-label btn-select">
-            画像を選択 <i class="fas fa-upload"></i>
-        </label>
-        {{-- ファイル名表示 --}}
-        <input type="file" name="imageUpload" id="imageUpload" accept="image/*" class="d-none" onchange="displayFileName()">
-        <span id="fileName" class="ms-2 file-name"></span>
-        {{-- 登録ボタン --}}
-        <button type="submit" class="btn btn-store ms-2">登録</button>
-    </div>  
-    <div class="row" id="gallery">
-        <div class="col-md-4 mb-4">
-            <div class="card">
-                <img src="path/to/image1.jpg" class="card-img-top" alt="Image 1" data-bs-toggle="modal" data-bs-target="#photoModal" onclick="showImage('path/to/image1.jpg')">
-                <div class="card-body">
-                    <h5 class="card-title">タイトル 1</h5>
-                </div>
-            </div>
-        </div>
-        <!-- 他の画像も同様に追加 -->
-    </div>
 </div>
+
 
 <!-- モーダル -->
 <div class="modal fade" id="photoModal" tabindex="-1" aria-labelledby="phptpModalLabel" aria-hidden="true">
