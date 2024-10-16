6bc1c5d32f5f1d17ed6ebfaeb405b1692c114d36
soga-kimika <170947386+soga-kimika@users.noreply.github.com>
2024-10-16 09:37:12 +0900

    change https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,5 +20,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
+        if(\App::environment(['production'])){
+            \URL::forceScheme('https'); 
+        }
+        
     }
 }
