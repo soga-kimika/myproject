8323a553782cf6d29da7bdc8c83338bd64471ac1
soga-kimika <170947386+soga-kimika@users.noreply.github.com>
2024-10-22 22:42:47 +0900

    ページリンク作成

--- resources/views/admin/homeStartupItems.blade.php
+++ resources/views/admin/homeStartupItems.blade.php
@@ -59,12 +59,15 @@
             @endforeach
         </tbody>
     </table>
-
+{{-- ページネーションリンク --}}
+{{ $homeStartupItems->links('vendor.pagination.bootstrap-4') }}
 </div>
 @endsection
 
 @section('css')
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 <link rel="stylesheet" href="{{ asset('/css/item/index.css') }}">
+
 <style>
 
 </style>
--- resources/views/admin/users.blade.php
+++ resources/views/admin/users.blade.php
@@ -49,21 +49,20 @@
                 @endforeach 
             </tbody>
         </table>
-        <nav aria-label="Page navigation">
             <ul class="pagination">
-              {{ $users->links() }}
+              {{ $users->links('vendor.pagination.bootstrap-4') }}
             </ul>
-          </nav>
           
 
     </div>
     @endsection
     @section('css')
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="{{ asset('/css/item/index.css') }}">
+
     @stop
 
     @section('js')
-    <script>
-    </script>
+    <script src="https://cdn.tailwindcss.com"></script>
     @stop
 
